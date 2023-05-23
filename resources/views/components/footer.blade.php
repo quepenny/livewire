cc2d5d204ee74aa48f049e7c77c97b5d0dba1662
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2023-05-23 14:04:08 +0100

    Add footer.blade.php

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<section class="mt-8 flex flex-wrap justify-between items-center">
+    <p class="order-last text-sm text-gray-400">© {{ date('Y') }} {{ config('app.name') }}. All Rights Reserved.</p>
+</section>
