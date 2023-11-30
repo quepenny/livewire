080333c6430b0f8ddea9a2c536af99ce53c6b8a0
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2023-11-30 13:30:06 +0000

    Misc

--- resources/views/components/content/title-subtitle.blade.php
+++ resources/views/components/content/title-subtitle.blade.php
@@ -1,7 +1,7 @@
 <x-content>
     <div class="container mx-auto py-8 px-4">
         <div class="mb-4">
-            <h2 class="text-2xl font-bold font-heading">{{ $title }}</h2>
+            <h2 class="text-3xl font-bold font-heading">{{ $title }}</h2>
             <p class="text-sm text-gray-500 leading-loose">{{ $subtitle }}</p>
         </div>
 
--- src/View/Components/Tooltip.php
+++ src/View/Components/Tooltip.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Str;
-use Quepenny\Livewire\View\Components\BaseComponent;
+use Quepenny\Livewire\BaseComponent;
 
 class Tooltip extends BaseComponent
 {
