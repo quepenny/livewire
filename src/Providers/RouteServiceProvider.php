554aaa1688a33632a6afd320f9e55e7e7fa3d0b4
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2024-03-23 11:41:19 +0000

    Update RouteServiceProvider.php Ensure all web routes use the wed middleware.

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace Quepenny\Livewire\Providers;
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 use Quepenny\Livewire\Http\Middleware\GuestToken;
 
 class RouteServiceProvider extends ServiceProvider
@@ -11,6 +12,9 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         app(Kernel::class)->appendMiddlewareToGroup('web', GuestToken::class);
-        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+
+        $this->routes(function () {
+            Route::middleware('web')->group(__DIR__.'/../../routes/web.php');
+        });
     }
 }
