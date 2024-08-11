3e7a5226f9f8ff1d6314bb343eb8b7d6a608f9af
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2024-08-11 18:59:46 +0100

    Update Settings page - Make title dynamic - Add nav object with settings set as active

--- src/Settings.php
+++ src/Settings.php
@@ -8,10 +8,12 @@ use Livewire\Attributes\Title;
 
 class Settings extends PageComponent
 {
-    #[Title('Settings')]
     #[Layout('components.layouts.guest')]
     public function render(): View
     {
-        return view('livewire.settings');
+        return view('livewire.settings')->layoutData([
+            'title' => __('pages.settings.title') ?: 'Settings',
+            'nav' => ['active' => 'settings'],
+        ]);
     }
 }
