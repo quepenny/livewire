84521051092081c9f79db48dd66625c982fd4f44
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2024-08-31 14:41:40 +0100

    Update MacroServiceProvider.php - Add Testable::assertModalDispatched

--- src/Providers/MacroServiceProvider.php
+++ src/Providers/MacroServiceProvider.php
@@ -2,19 +2,26 @@
 
 namespace Quepenny\Livewire\Providers;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Scout\Builder;
+use Livewire\Features\SupportTesting\Testable;
+use PHPUnit\Framework\Assert as PHPUnit;
+use Quepenny\Livewire\Modal\BaseModalComponent;
+use Quepenny\Livewire\Modal\Builders\BaseModalBuilder;
 
 class MacroServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->scoutMacros();
-        $this->stringMacros();
+        $this->scout();
+        $this->string();
+        $this->livewireTestable();
     }
 
-    private function stringMacros(): void
+    private function string(): void
     {
         Str::macro('space', function (string $text) {
             if (! ctype_lower($text)) {
@@ -26,7 +33,7 @@ class MacroServiceProvider extends ServiceProvider
         });
     }
 
-    private function scoutMacros(): void
+    private function scout(): void
     {
         Builder::macro('buildQuery', function (callable $callback) {
             if (is_callable($this->queryCallback)) {
@@ -39,6 +46,29 @@ class MacroServiceProvider extends ServiceProvider
             } else {
                 $this->queryCallback = $callback;
             }
+
+            return $this;
+        });
+    }
+
+    private function livewireTestable(): void
+    {
+        Testable::macro('assertModalDispatched', function (BaseModalComponent|BaseModalBuilder $modal, $params = []) {
+            $eventParams = [
+                'component' => $modal::slug(),
+                'arguments' => $modal instanceof Arrayable ? $modal->toArray() : $params,
+            ];
+
+            PHPUnit::assertTrue(
+                $this->testDispatched('openModal', $eventParams)['test'],
+                "Failed asserting that {$modal::slug()} was dispatched with the provided parameters."
+            );
+
+            PHPUnit::assertSame(
+                Session::get('open-modal'),
+                $eventParams,
+                "Failed asserting that {$modal::slug()} was dispatched using a session variable."
+            );
 
             return $this;
         });
