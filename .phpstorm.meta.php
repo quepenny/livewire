5b9ccf40d9d10bb04b5d2616eefcc3e42ef4d5f9
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2024-09-07 20:45:11 +0100

    Update .phpstorm.meta.php - Add Testable::assertToastDispatched

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,8 +3,11 @@
 // This file is for PhpStorm autocompletion purposes only.
 
 namespace Livewire\Features\SupportTesting {
+    use Quepenny\Livewire\Modal\BaseModalComponent;
+    use Quepenny\Livewire\Modal\Builders\BaseModalBuilder;
+
     /**
-     * @method \Livewire\Features\SupportTesting\Testable assertModalDispatched(string $modalClass, array $params = [])
+     * @method \Livewire\Features\SupportTesting\Testable assertModalDispatched(BaseModalComponent|BaseModalBuilder $modalClass, array $params = [])
      * @method \Livewire\Features\SupportTesting\Testable assertToastDispatched(string $mode, string $message)
      */
     class Testable
