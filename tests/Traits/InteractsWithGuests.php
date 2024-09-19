08f6d804c33d3a5ca87c20a120a4c90a5bf86d04
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2024-09-19 07:12:17 +0100

    Add InteractsWithGuests::actingAsMember

--- tests/Traits/InteractsWithGuests.php
+++ tests/Traits/InteractsWithGuests.php
@@ -3,6 +3,7 @@
 namespace Quepenny\Livewire\Tests\Traits;
 
 use App\Models\Guest;
+use App\Models\User;
 use Illuminate\Support\Facades\Session;
 
 trait InteractsWithGuests
@@ -10,6 +11,13 @@ trait InteractsWithGuests
     protected function actingAsGuest(Guest $guest = null): static
     {
         Session::put('guest', $guest ?? Guest::query()->create());
+
+        return $this;
+    }
+
+    protected function actingAsMember(User $user = null): static
+    {
+        $this->actingAs($user ?: User::factory()->create());
 
         return $this;
     }
