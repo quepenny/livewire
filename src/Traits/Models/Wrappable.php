536c54dd448969d82fa529b566de95f4639ea31a
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2024-11-24 16:06:18 +0000

    Update Wrappable.php - Allow custom model resolver callback.

--- src/Traits/Models/Wrappable.php
+++ src/Traits/Models/Wrappable.php
@@ -4,12 +4,14 @@ namespace Quepenny\Livewire\Traits\Models;
 
 trait Wrappable
 {
-    public static function wrap(mixed $model): ?static
+    public static function wrap(mixed $model, ?callable $resolver = null): ?static
     {
         if (! $model || $model instanceof static) {
             return $model;
         }
 
-        return static::find($model);
+        $resolver = $resolver ?: fn ($model) => static::find($model);
+
+        return $resolver($model);
     }
 }
