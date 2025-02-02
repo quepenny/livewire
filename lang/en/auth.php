5ad565d406de5a3f6729abbe715f1b8fb4786402
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2025-02-02 14:44:39 +0000

    Update auth.disclaimer-data-transfer lang - Make it clearer that it's guest data being transferred.

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'disclaimer-data-transfer' => 'Note: All your saved data will get transferred to this account.',
+    'disclaimer-data-transfer' => 'Note: All your saved guest data will get transferred to this account.',
     'forgot-password' => 'No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
     'confirm-password' => 'This is a secure area of the application. Please confirm your password before continuing.',
 ];
