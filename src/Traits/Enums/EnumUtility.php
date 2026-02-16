7da1a11ac9c6bba6b3ea936d6c2ef3c8a211448d
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2026-02-16 00:20:15 +0000

    Add `values` method to `EnumUtility` trait to retrieve all enum values

--- src/Traits/Enums/EnumUtility.php
+++ src/Traits/Enums/EnumUtility.php
@@ -4,6 +4,11 @@ namespace Quepenny\Livewire\Traits\Enums;
 
 trait EnumUtility
 {
+    public static function values(): array
+    {
+        return array_map(fn($case) => $case->value, self::cases());
+    }
+
     public static function dropdownOptions(string $placeholder = '', array $additional = []): array
     {
         return enum_dropdown_options(self::cases(), $placeholder, $additional);
