f763c09cc77121892a70877b92bbe435bfca8061
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2026-02-27 19:26:24 +0000

    Integrate `EnumUtility` trait into `UserType` enum for enhanced functionality.

--- src/Enums/UserType.php
+++ src/Enums/UserType.php
@@ -2,7 +2,11 @@
 
 namespace Quepenny\Livewire\Enums;
 
+use Quepenny\Livewire\Traits\Enums\EnumUtility;
+
 enum UserType: string {
+    use EnumUtility;
+
     case MEMBER = 'member';
     case GUEST = 'guest';
 }
