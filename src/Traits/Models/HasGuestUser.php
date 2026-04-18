d0c8c6208865e5eff3e095ee6895ddab69e51a68
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2026-04-18 18:36:54 +0100

    Add casting for `user_type` using `UserType` enum in `HasGuestUser` trait for type consistency.

--- src/Traits/Models/HasGuestUser.php
+++ src/Traits/Models/HasGuestUser.php
@@ -5,9 +5,15 @@ namespace Quepenny\Livewire\Traits\Models;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Quepenny\Livewire\Enums\UserType;
 
 trait HasGuestUser
 {
+    public function casts(): array
+    {
+        return [ 'user_type' => UserType::class ];
+    }
+
     public function user(): MorphTo
     {
         return $this->morphTo();
