014452f19de97f3865e3924b48172da5a461d9d8
Paul Ogbeiwi <paul.ogbeiwi@quepenny.com>
2026-06-05 08:24:43 +0100

    Add EnquiryFactory and HasFactory trait to Enquiry model for factory support.

---
+++ database/factories/EnquiryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Quepenny\Livewire\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Quepenny\Livewire\Models\Enquiry;
+
+/**
+ * @extends Factory<Enquiry>
+ */
+class EnquiryFactory extends Factory
+{
+    protected $model = Enquiry::class;
+
+    public function definition(): array
+    {
+        return [
+            'name'    => fake()->name(),
+            'email'   => fake()->safeEmail(),
+            'subject' => fake()->sentence(4),
+            'message' => fake()->paragraph(),
+        ];
+    }
+}
--- src/Models/Enquiry.php
+++ src/Models/Enquiry.php
@@ -2,11 +2,13 @@
 
 namespace Quepenny\Livewire\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
 class Enquiry extends Model
 {
+    use HasFactory;
     use Notifiable;
 
     protected $guarded = [];
